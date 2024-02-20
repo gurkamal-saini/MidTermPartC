6c51b054b642b494297578ffc8ef8058810fafbf
Gurkamal Singh <sin14371@shernet.sheridancollege.ca>
2024-02-20 12:22:54 -0500

    Created Java class named as Testuserprofile

---
+++ src/TestUserprofile.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+
+    import java.util.Scanner;
+
+public class TestUserprofile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Gurkamal");
+        String name = scanner.nextLine();
+
+        System.out.println("Comedy");
+        String[] genres = {"Action", "Romance", "Horror", "Sci-Fi", "Comedy"};
+        for (int i = 0; i < genres.length; i++) {
+            System.out.println((i + 1) + ". " + genres[i]);
+        }
+
+        int genreChoice = scanner.nextInt();
+        while (genreChoice < 1 || genreChoice > genres.length) {
+            System.out.println("Invalid choice, please choose a number between 1 and " + genres.length);
+            genreChoice = scanner.nextInt();
+        }
+        String favouriteGenre = genres[genreChoice - 1];
+
+        System.out.println("UserProfile created!");
+        System.out.println("Name:" + name);
+        System.out.println("Favourite Genre: " + favouriteGenre);
+        
+        scanner.close();
+    }
+}
+
+
